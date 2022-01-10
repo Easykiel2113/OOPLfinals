c46ac08c51a5aa0073e6c8d61296fd36eda120ec
Ezekiel Ortiguerra <zenkiortiguerra@gmail.com>
2022-01-10 14:50:26 +0800

    asdasd

--- index.php
+++ index.php
@@ -1,1 +1,166 @@
-asdasd
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@Easykiel2113 
+Easykiel2113
+/
+OOPLmidterm
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+OOPLmidterm/s.php /
+@Easykiel2113
+Easykiel2113 i
+Latest commit 513a490 on Nov 20, 2021
+ History
+ 1 contributor
+137 lines (106 sloc)  3.07 KB
+   
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOPL</title>
+</head>
+<body>
+<?php
+Interface  Patient {
+	public function GeneralInfo();
+
+}
+
+class generalinfo {
+	public $name;
+	public $age;
+	public $gender;
+	public function __construct($name,$age,$gender,$address) {
+		$this -> name = $name;
+		$this -> age = $age;
+	    $this -> gender = $gender;
+		$this -> address = $address;
+	   
+	}
+}
+class Status {
+    public $status;
+    public function __construct($status){
+        $this -> status = $status;
+    }
+}
+
+class sta extends Status implements Patient{
+    function GeneralInfo(){
+        echo 'Status:'. $this -> status;
+    }
+}
+
+class info extends generalinfo  implements Patient {
+	function GeneralInfo() {
+		echo 'Name:'. $this -> name .'  '.' Age:'. $this -> age .' '. 'Gender:' . $this -> gender.' '. 'Address: '. $this -> address ;
+	  
+	
+	}
+}
+class info1 extends generalinfo  implements Patient {
+	function GeneralInfo() {
+		echo 'Name:'. $this -> name .'  '.' Age:'. $this -> age .' '. 'Gender:' . $this -> gender .' '.'Address: '.$this -> address;
+	  
+	
+	}
+}
+class info2 extends generalinfo  implements Patient {
+	function GeneralInfo() {
+		echo 'Name:'. $this -> name .'  '.' Age:'. $this -> age .' '. 'Gender:' . $this -> gender .' '.' Address: '. $this -> address;
+	  
+	
+	}
+}
+class info3 extends generalinfo  implements Patient {
+	function GeneralInfo() {
+		echo 'Name:'. $this -> name .'  '.' Age:'. $this -> age .' '. 'Gender:' . $this -> gender .' '.'Address: '. $this -> address;
+	  
+	
+	}
+}
+class info4 extends generalinfo  implements Patient {
+	function GeneralInfo() {
+		echo 'Name:'. $this -> name .'  '.' Age:'. $this -> age .' '. 'Gender:' . $this -> gender .' '.'Address: '. $this -> address;
+	  
+	
+	}
+}
+
+
+
+$Info = new info('Hyvor',12,'male','Abucay, Bataan');
+echo "Date of consultation:" . date("m/d/y") . "<br>";
+echo "Infomation:". "<br>";
+$Info -> GeneralInfo();
+echo "<br>";
+$sta = new sta('Done');
+$sta ->GeneralInfo();
+echo "<br>";
+echo "<br>";
+
+$Info = new info1('Ezekiel',13,'male','Samal, Bataan');
+echo "Date of consultation:" . date("m/d/y") . "<br>";
+echo "Infomation:". "<br>";
+$Info -> GeneralInfo();
+echo "<br>";
+$sta = new sta('Done');
+$sta ->GeneralInfo();
+
+
+echo "<br>";
+echo "<br>";
+$Info = new info2('Tyron  ',18,'male','Orani, Bataan');
+echo "Date of consultation:" . date("m/d/y") . "<br>";
+echo "Infomation:". "<br>";
+$Info -> GeneralInfo();
+echo "<br>";
+$sta = new sta('Done');
+$sta ->GeneralInfo();
+echo "<br>";
+echo "<br>";
+
+
+$Info = new info3('Diana rose',18,'female','Balanga City, Bataan');
+echo "Date of consultation:" . date("m/d/y") . "<br>";
+echo "Infomation:". "<br>";
+$Info -> GeneralInfo();
+echo "<br>";
+$sta = new sta('Done');
+$sta ->GeneralInfo();
+echo "<br>";
+echo "<br>";
+
+$Info = new info4('kei',18,'female','Limay, Bataan');
+echo "Date of consultation:" . date("m/d/y") . "<br>";
+echo "Infomation:". "<br>";
+$Info -> GeneralInfo();
+echo "<br>";
+$sta = new sta('Done');
+$sta ->GeneralInfo();
+
+
+
+
+?>
+
+
+</body>
+</html>
