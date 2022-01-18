14362157f5c0e2b393fef2bc3e5c09ded785a151
Ezekiel Ortiguerra <zenkiortiguerra@gmail.com>
2022-01-19 00:31:50 +0800

    asdas

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(205,182,0,1) 0%, rgba
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Report page</title>
+    <title>Clinic system information board</title>
 </head>
 <body>
     
